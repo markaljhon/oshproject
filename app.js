81f20cebaf52ef873ae9b9421ce38fe4aed7751c
markaljhon <icmarkaljhon@gmail.com>
2017-10-27 15:25:33 +0800

    Fix variable error

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.post('/webhook', (req, res) => {
             console.log(webhookEvent);
 
             // Get the sender PSID
-            let sender_psid = webhook_event.sender.id;
+            let sender_psid = webhookEvent.sender.id;
             console.log('Sender PSID: ' + sender_psid);
         });
 
