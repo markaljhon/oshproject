730b4066734991c8603dc3a60f50059740d98982
markaljhon <icmarkaljhon@gmail.com>
2017-10-27 16:32:25 +0800

    Fix.

--- app.js
+++ app.js
@@ -109,7 +109,7 @@ function callSendAPI(sender_psid, response) {
     let PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
     // Construct the message body
-    let message = {
+    let request_body = {
         "recipient": {
             "id": sender_psid
         },
