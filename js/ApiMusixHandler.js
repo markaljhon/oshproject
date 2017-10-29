128ba04bfd2d19506430d796300a60d1a8d799d4
markaljhon <icmarkaljhon@gmail.com>
2017-10-30 01:16:53 +0800

    Fix logs.

--- js/ApiMusixHandler.js
+++ js/ApiMusixHandler.js
@@ -33,15 +33,15 @@ const sendResult = (senderPSID, jsonMusix) => {
     let strResponse = "";
     let response;
 
-    // jsonMusix.message.body.track_list.forEach((track, index) =>{
-    //     strResponse += `${index}. ${track.track_name}.\n`;
-    // });
+    jsonMusix.message.body.track_list.forEach((track, index) =>{
+        strResponse += `${index}. ${track.track_name}.\n`;
+    });
 
-    for (var i = 0; i < jsonMusix.message.header.available; i++) {
-        strResponse += `${i}. ${jsonMusix.message.body.track_list[i].track.track_name}}.\n`;
-    }
+    // for (var i = 0; i < jsonMusix.message.header.available; i++) {
+    //     strResponse += `${i}. ${jsonMusix.message.body.track_list[i].track.track_name}}.\n`;
+    // }
 
-    console.log(`APP:: Musixmatch: Response: ${strResponse}`)
+    console.log(`APP:: Musixmatch: (${jsonMusix.message.header.available}) Response: ${strResponse}`)
 
     response = { "text": strResponse }
     requestHandler(senderPSID, response);
