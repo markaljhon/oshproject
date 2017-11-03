f9a238049f0c578c186d084f92e0335e3d6b338f
markaljhon <icmarkaljhon@gmail.com>
2017-11-03 16:18:05 +0800

    Add promise.

--- js/Musixmatch.js
+++ js/Musixmatch.js
@@ -6,20 +6,22 @@ const request = require('request');
 const Musixmatch = (options) => {
   let result = '';
 
-  request(options, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-      console.log(`Musixmatch: Track Received: (${body.message.header.available} tracks) 1st - "${body.message.body.track_list[0].track.track_name}"`);
+  return new Promise(resolve => {
+    request(options, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        console.log(`Musixmatch: Track Received: (${body.message.header.available} tracks) 1st - "${body.message.body.track_list[0].track.track_name}"`);
 
-      body.message.body.track_list.forEach((track, index) => {
-        result += `${index + 1}. ${body.message.body.track_list[index].track.track_name}.\n`;
-      });
-      console.log(`Musixmatch Result: ${result}`);
+        body.message.body.track_list.forEach((track, index) => {
+          result += `${index + 1}. ${body.message.body.track_list[index].track.track_name}.\n`;
+        });
+        console.log(`Musixmatch Result: ${result}`);
 
-      return result;
-    } else {
-      console.error(`Musixmatch: API request not sent. (Status ${response.statusCode}) (${error})`);
-    }
-  });
+        resolve(result);
+      } else {
+        console.error(`Musixmatch: API request not sent. (Status ${response.statusCode}) (${error})`);
+      }
+    }); // End of callback and request.
+  }); // End of Promise.
 };
 
 module.exports = Musixmatch;
